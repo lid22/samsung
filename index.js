f2a5e433de771c8e556a71781e08b224266c7cf7
lid22 <125208237+lid22@users.noreply.github.com>
2023-02-19 23:19:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const email = document.getElementById("email");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("¡Se esperaba una dirección de correo electrónico!");
+  } else {
+    email.setCustomValidity("");
+  }
+
+
+function valida_envia(){
+    console.log('Formulario enviado')}
+ 
+var nombre = document.getElementsByld("nombre")
+{
+    if (document.getElementByld("input")="[A-Za-z]")
+    document.getElementByld("input)".style.border-color== "green");
+
+else
+
+document.getElementByld("input").style.border-color== "red");
+}
+{
+    input[0].style.border-color = 'red';
+    
+    input[1].style.border-color = 'green';
+    
+}
